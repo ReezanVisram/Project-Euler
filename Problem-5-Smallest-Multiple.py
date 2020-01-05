fd6ca1283d7ab827a9ef49867e4d988107fc5320
ReezanVisram <59345266+ReezanVisram@users.noreply.github.com>
2020-01-05 17:24:37 -0500

    Updating the solved problem 5.

--- Problem-5-Smallest-Multiple.py
+++ Problem-5-Smallest-Multiple.py
@@ -1,28 +1,11 @@
-# UNSOLVED
-def factorial(n):
-    if n == 1:
-        return n
+# Project Euler Problem 5
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+# Starts off the counter at 20
+i = 20
 
-    else:
-        return n * factorial(n - 1)
+""" Loops until the number is divisible by everything between 1 and 20 (Didn't have to check all numbers because if that number times 2 is less than 20,
+and a number is divisible by that product, it is automatically divisible by the inital number. E.g if a number is divisible by 20, it is automatically divisible by 10 """
+while (i % 11 != 0) or (i % 12 != 0)  or (i % 13 != 0) or (i % 14 != 0) or (i % 15 != 0) or (i % 16 != 0) or (i % 17 != 0) or (i % 18 != 0) or (i % 19 != 0) or (i % 20 != 0):
+    i += 20
 
-def factors(x):
-    result = []
-
-    i = 1
-
-    while i ** 2 <= x:
-        if (x % i == 0):
-            result.append(i)
-
-        if (x // i != i):
-            result.append(x//i)
-
-        i += 1
-
-    return result
-
-fact20 = factorial(20)
-print(fact20)
-
-print(factors(fact20) / 380)
+print(i)
